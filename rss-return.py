4602c89c31a76193ed14d1fb8092e6e4926bccc0
Austin Moore <am058613@ohio.edu>
2018-03-28 02:37:23 -0400

    Fixed being able to output to file when not being able to find RSS

--- rss-return.py
+++ rss-return.py
@@ -10,6 +10,7 @@ while (ans == 'y' or ans == 'Y'):
     req = urllib.request.Request(url)
     resp = urllib.request.urlopen(req)
     respData = resp.read().decode("utf-8")
+    unable_to_find = False
 
     try:
         rss = re.findall(r'channel_id=(.*)"', respData)
@@ -17,12 +18,14 @@ while (ans == 'y' or ans == 'Y'):
         print(rss)
         print()
     except:
+        unable_to_find = True
         print("Unable to find RSS")
         print()
 
-    ans = input("Would you like to output URL to output.txt? ('y' or 'n'): ")
-    if (ans == 'Y' or ans == 'y'):
-        print(rss, file=open("output.txt", "a"))
+    if (unable_to_find == False):
+        ans = input("Would you like to output URL to output.txt? ('y' or 'n'): ")
+        if (ans == 'Y' or ans == 'y'):
+            print(rss, file=open("output.txt", "a"))
 
     ans = input("Would you like to enter another url? ('y' or 'n'): ")
     print()
