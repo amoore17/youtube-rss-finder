7ec5b15efe1e1c809fff2b2b5ffacb5b8c13ca34
Austin Moore <am058613@ohio.edu>
2018-03-28 02:23:32 -0400

    Wrote file for parsing the RSS feed for YouTube channels

---
+++ rss-return.py
@@ -0,0 +1,28 @@
+import urllib.request
+import re
+
+print()
+
+ans = 'y'
+
+while (ans == 'y' or ans == 'Y'):
+    url = input("Enter YouTube URL: ")
+    req = urllib.request.Request(url)
+    resp = urllib.request.urlopen(req)
+    respData = resp.read().decode("utf-8")
+
+    try:
+        rss = re.findall(r'channel_id=(.*)"', respData)
+        rss = "https://www.youtube.com/feeds/videos.xml?channel_id=" + rss[0]
+        print(rss)
+        print()
+    except:
+        print("Unable to find RSS")
+        print()
+
+    ans = input("Would you like to output URL to output.txt? ('y' or 'n'): ")
+    if (ans == 'Y' or ans == 'y'):
+        print(rss, file=open("output.txt", "a"))
+
+    ans = input("Would you like to enter another url? ('y' or 'n'): ")
+    print()
